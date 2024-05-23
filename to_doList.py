1fcc89709e63637d5a1248199d85f49b861a9df7
Ayush6196 <ayushshukla6196@gmail.com>
2024-05-23 19:27:12 +0530

    file-input

--- to_doList.py
+++ to_doList.py
@@ -3,6 +3,8 @@ import os
 
 
 def main():
+    file_path = "todo_list.txt"
+    tasks = load_task_from_file(file_path)
     print("\n=======To-Do-List=======")
     print("1.Show Tasks")
     print("2.Add Tasks")
@@ -10,6 +12,11 @@ def main():
     print("4.Delete Tasks")
     print("5.Save and Exit")
     print("1.Show Tasks")
+    choice = input("Enter your Choice(1-5): ")
+    if choice =="1":
+        show_tasks(tasks)
+    elif choice =="2"
+    
 
 if _name_ == "_main_" :
     main()
