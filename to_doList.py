af3c3c190ca3f597bcdd4d02a03b0cd56c45cc7d
Ayush6196 <ayushshukla6196@gmail.com>
2024-05-23 19:15:03 +0530

    define-main-function

---
+++ to_doList.py
@@ -0,0 +1,15 @@
+import os
+
+
+
+def main():
+    print("\n=======To-Do-List=======")
+    print("1.Show Tasks")
+    print("2.Add Tasks")
+    print("3.Update Tasks")
+    print("4.Delete Tasks")
+    print("5.Save and Exit")
+    print("1.Show Tasks")
+
+if _name_ == "_main_" :
+    main()
