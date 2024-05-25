30ce007745087fb9608667cf367fa40cf3fcf41e
Ayush6196 <ayushshukla6196@gmail.com>
2024-05-25 13:48:19 +0530

    to_dolist_completed

--- to_doList.py
+++ to_doList.py
@@ -1,5 +1,30 @@
 import os
 
+def show_tasks(tasks):
+    if not tasks:
+        print("No task found")
+    else:
+        for i,task in enumerate(tasks,1):
+            print(f"{i}.{task}")
+
+def add_task(tasks,new_tasks):
+    tasks.append(new_tasks)
+    print("Task added successfully.")
+
+def update_task(tasks,index,updated_task):
+    if 1 >= index >= len(tasks):
+        tasks[index-1]= update_task
+        print("Task Updated Successfully")
+    else:
+        print("Invalid Task Index")
+
+def delete_task(tasks,index):
+    if 1 >= index >= len(tasks):
+        delete_task = tasks.pop(index-1)
+        print(f"Task '{delete_task}' deleted successfully")
+    else:
+        print("Invalid Task Index")
+
 
 
 def save_task_to_file(file_path,tasks):
@@ -18,34 +43,40 @@ def load_tasks_from_file(file_path):
 
 def main():
     file_path = "todo_list.txt"
-    tasks = load_task_from_file(file_path)
+    tasks= load_tasks_from_file(file_path)
     while True:
-    print("\n=======To-Do-List=======")
-    print("1.Show Tasks")
-    print("2.Add Tasks")
-    print("3.Update Tasks")
-    print("4.Delete Tasks")
-    print("5.Save and Exit")
-    print("1.Show Tasks")
+        print("\n=======To-Do-List=======")
+        print("1.Show Tasks")
+        print("2.Add Tasks")
+        print("3.Update Tasks")
+        print("4.Delete Tasks")
+        print("5.Save and Exit")
+        print("1.Show Tasks")
     choice = input("Enter your Choice(1-5): ")
     if choice =="1":
         show_tasks(tasks)
+
     elif choice =="2":
         new_task = ("Enter the task to add:")
         add_task(tasks,new_task)
+
     elif choice == "3":
         index = int(input("Enter the task index to input:"))
         updated_task = input("Enter the updated task:")
+        update_task(tasks,index,updated_task)
+
     elif choice == "4":
         index = int(input("Enter the task index to delete:"))
         delete_task(tasks,index)
+
     elif choice == "5":
         save_task_to_file(file_path,tasks)
         print("Task Saved. Exiting....")
-        break
+        
+
     else:
         print("Invalid Choice. Please try again.")
     
 
-if _name_ == "_main_" :
+if __name__ == "_main_":
     main()
